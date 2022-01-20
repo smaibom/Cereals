713f61a042c7458c245fa8b5879415ed6f485f9c
simon <smaibom@gmail.com>
2022-01-20 13:08:22 +0100

    removed config

--- cerealWebapp/config.py
+++
@@ -1,5 +0,0 @@
-import urllib
-secret = 'secret-key-goes-here'
-dbstring = 'DRIVER={SQL Server};SERVER=localhost;PORT=1433;DATABASE=cereals;UID=sa;PWD=Password1!'
-params = urllib.parse.quote_plus(dbstring)
-db_uri = "mssql+pyodbc:///?odbc_connect=%s" % params
